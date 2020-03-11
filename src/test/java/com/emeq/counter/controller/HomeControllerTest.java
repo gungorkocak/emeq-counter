98d3847f9bd35d640a6e899428b5d3e7fb95b51a
Furkan Ural <uralfrkn@gmail.com>
2020-03-11 11:57:26 +0300

    testing `/hello` returns to `{"hello": "world"}`

--- src/test/java/com/emeq/counter/controller/HomeControllerTest.java
+++ src/test/java/com/emeq/counter/controller/HomeControllerTest.java
@@ -2,18 +2,42 @@ package com.emeq.counter.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
+
+  @LocalServerPort
+  private int port;
 
   @Autowired
   private HomeController controller;
 
+  @Autowired
+  private TestRestTemplate restTemplate;
+
   @Test
   public void contexLoads() throws Exception {
     assertThat(controller).isNotNull();
+  }
+
+  @Test
+  public void indexShouldReturnHelloWorld() throws Exception {
+    String endpoint = "http://localhost:" + port + "/hello";
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode json = mapper.createObjectNode();
+    ((ObjectNode)json).put("hello", "world");
+
+    assertThat(this.restTemplate.getForObject(endpoint, JsonNode.class))
+        .isEqualTo(json);
   }
 }
