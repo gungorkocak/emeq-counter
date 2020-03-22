4dd6df83c953acc83d847a6e7747cf6e20627c3f
Furkan Ural <uralfrkn@gmail.com>
2020-03-22 16:19:25 +0300

    changed space size to 4

--- src/test/java/com/emeq/counter/controller/HomeControllerTest.java
+++ src/test/java/com/emeq/counter/controller/HomeControllerTest.java
@@ -2,6 +2,7 @@ package com.emeq.counter.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.emeq.counter.HelloController;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -16,28 +17,27 @@ import org.springframework.boot.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
 
-  @LocalServerPort
-  private int port;
+    @LocalServerPort
+    private int port;
 
-  @Autowired
-  private HomeController controller;
+    @Autowired
+    private HelloController controller;
 
-  @Autowired
-  private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-  @Test
-  public void contexLoads() throws Exception {
-    assertThat(controller).isNotNull();
-  }
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
 
-  @Test
-  public void indexShouldReturnHelloWorld() throws Exception {
-    String endpoint = "http://localhost:" + port + "/hello";
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode json = mapper.createObjectNode();
-    ((ObjectNode)json).put("hello", "world");
+    @Test
+    public void indexShouldReturnHelloWorld() throws Exception {
+        String endpoint = "http://localhost:" + port + "/hello";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.createObjectNode();
+        ((ObjectNode) json).put("hello", "world");
 
-    assertThat(this.restTemplate.getForObject(endpoint, JsonNode.class))
-        .isEqualTo(json);
-  }
+        assertThat(this.restTemplate.getForObject(endpoint, JsonNode.class)).isEqualTo(json);
+    }
 }
