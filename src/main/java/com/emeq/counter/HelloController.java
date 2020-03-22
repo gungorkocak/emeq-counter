c9e403d42dd25d3464e410b234525b0c538e14bc
Burcak Kose <burcakose@gmail.com>
2020-03-22 14:07:38 +0100

    added Hello endpoint. (#10)
    * added Hello endpoint.
    
    * [ch20] updated hello world endpoint to comply with ac.
    
    * update indentation.
    
    * force indentation.
    
    * remove unused import in HomeController.
    
    Co-authored-by: gungorkocak <gngr.kck@gmail.com>

---
+++ src/main/java/com/emeq/counter/HelloController.java
@@ -0,0 +1,13 @@
+package com.emeq.counter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Hello hello() {
+        return new Hello();
+    }
+}
