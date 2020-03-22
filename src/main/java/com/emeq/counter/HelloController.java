52dce0df932c231eb5fc30c145be4c47e815484b
gungorkocak <gngr.kck@gmail.com>
2020-03-22 13:59:10 +0100

    remove unused import in HomeController.

--- src/main/java/com/emeq/counter/HelloController.java
+++ src/main/java/com/emeq/counter/HelloController.java
@@ -1,7 +1,6 @@
 package com.emeq.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
