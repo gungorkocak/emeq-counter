bb65d9547e216af1384b5dc6487a369823e1fc40
Furkan Ural <uralfrkn@gmail.com>
2020-04-15 20:04:16 +0300

    created userEntity with email and encryptedPassword fields

---
+++ backend/src/main/java/com/emeq/counter/model/User.java
@@ -0,0 +1,28 @@
+package com.emeq.counter.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
+@Entity(name = "users")
+@Table(
+    name = "users",
+    indexes = { @Index(name="user_email_index", columnList = "email", unique = true) }
+)
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String encryptedPassword;
+}
