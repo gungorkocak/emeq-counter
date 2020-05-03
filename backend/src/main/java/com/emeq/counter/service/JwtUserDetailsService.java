6b460b943b2a6bb5991b020812f1620554ba6725
Furkan Ural <uralfrkn@gmail.com>
2020-05-03 16:43:44 +0300

    import missing packages

--- backend/src/main/java/com/emeq/counter/service/JwtUserDetailsService.java
+++ backend/src/main/java/com/emeq/counter/service/JwtUserDetailsService.java
@@ -2,9 +2,12 @@ package com.emeq.counter.service;
 
 import com.emeq.counter.dto.UserDTO;
 import com.emeq.counter.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 @Service
 public class JwtUserDetailsService {
 
