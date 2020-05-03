10dfdf22c66fd1ea55aba2d13b8e286d9501db2f
Furkan Ural <uralfrkn@gmail.com>
2020-05-03 16:42:25 +0300

    import missings packages

--- backend/src/main/java/com/emeq/counter/controller/JwtAuthenticationController.java
+++ backend/src/main/java/com/emeq/counter/controller/JwtAuthenticationController.java
@@ -1,12 +1,16 @@
 package com.emeq.counter.controller;
 
 import javax.validation.Valid;
+
 import com.emeq.counter.dto.UserDTO;
 import com.emeq.counter.service.JwtUserDetailsService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -19,3 +23,4 @@ public class JwtAuthenticationController {
     public ResponseEntity<Object> signUp(@Valid @RequestBody UserDTO request) {
         return userDetailsService.create(request);
     }
+}
